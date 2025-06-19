8f68bb56d424adee42142d8ba48ab8b7a7a1ce31
Vasiliy Kirichenko <131616200+VaskaDaGama26@users.noreply.github.com>
2025-06-20 01:04:21 +0300

    fix(WorldMap): solved icon-marker loading error

--- src/pages/FoodPage/WorldMap/WorldMap.tsx
+++ src/pages/FoodPage/WorldMap/WorldMap.tsx
@@ -5,6 +5,19 @@ import './WorldMap.css';
 import type { DishProps } from '../types';
 import ImageWithSkeleton from '../../../components/ImageWithSkeleton';
 
+// ICONS LEAFLET
+import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 const WorldMap: React.FC<DishProps> = ({ data }) => {
   return (
     <MapContainer
